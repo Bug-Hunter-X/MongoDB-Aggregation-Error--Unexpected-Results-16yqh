8f6ac5e5b683dcb9c5ee23720c18e906f609f16a
BugHunter-X <askzenai@gmail.com>
2024-12-28 19:10:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,19 @@
+```javascript
+db.users.aggregate([
+  {
+    $match: {
+      age: {
+        $gt: 25
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$city",
+      averageAge: { $avg: "$age" },
+      totalUsers: { $sum: 1 }
+    }
+  },
+  { $sort: { averageAge: -1 } }
+]);
+```
